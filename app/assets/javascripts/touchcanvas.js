7eca960342656824345607b4efa6b0746df31f4b
EOdOW <artem@lyatiss.com>
2013-12-16 15:17:11 -0800

    Added a settimout function when the scan is a success, creates an scanning loop no matter what.

--- app/assets/javascripts/touchcanvas.js
+++ app/assets/javascripts/touchcanvas.js
@@ -17,19 +17,19 @@ var camhtml='<object  id="iembedflash" classid="clsid:d27cdb6e-ae6d-11cf-96b8-44
     '<param name="allowScriptAccess" value="always" />'+
     '<embed  allowScriptAccess="always"  id="embedflash" src="camcanvas.swf" quality="high" width="500" height="375" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" mayscript="true"  />'+
 '</object>';
+
 function isTrue(taken){
     return taken.replace(/^\s\s*/, '').replace(/\s\s*$/, '') === "true" ;
 }
+
 function read(jsonString) {
     var jsonObj = $.parseJSON(jsonString);
     var taken = $("." + jsonObj.serial_number + " .reserve").text();
+   
     if( isTrue(taken) ){
-        console.log("HERE", $("." + jsonObj.serial_number + " .reserve").text());
-
         jsonObj.user = "";
         submitQr(jsonObj);
     } else {
-        console.log("THERE", $("." + jsonObj.serial_number + " .reserve").text());
         $("#nameModal").modal();
 
         $(".js-submit").on("click", function() {
@@ -62,6 +62,7 @@ function load() {
         qrcode.callback = read;
         document.getElementById("webcamContainer").style.display="inline";
         setwebcam();
+
     } else {
         document.getElementById("webcamContainer").style.display="inline";
         document.getElementById("webcamContainer").innerHTML='<p id="mp1">QR code scanner for HTML5 capable browsers</p><br>'+
@@ -97,8 +98,7 @@ function error(error) {
 
 function setwebcam() {
     
-    if(stype==1)
-    {
+    if(stype==1) {
         setTimeout(captureToCanvas, 500);    
         return;
     }
@@ -142,29 +142,6 @@ function initCanvas(ww,hh) {
     imageData = gCtx.getImageData( 0,0,320,240);
 }
 
-function passLine(stringPixels) { 
-    //a = (intVal >> 24) & 0xff;
-
-    var coll = stringPixels.split("-");
-
-    for(var i=0;i<320;i++) { 
-        var intVal = parseInt(coll[i]);
-        r = (intVal >> 16) & 0xff;
-        g = (intVal >> 8) & 0xff;
-        b = (intVal ) & 0xff;
-        imageData.data[c+0]=r;
-        imageData.data[c+1]=g;
-        imageData.data[c+2]=b;
-        imageData.data[c+3]=255;
-        c+=4;
-    } 
-
-    if(c>=320*240*4) { 
-        c=0;
-            gCtx.putImageData(imageData, 0,0);
-    } 
-} 
-
 function captureToCanvas() {
     if(stype!=1)
         return;
@@ -174,6 +151,7 @@ function captureToCanvas() {
             gCtx.drawImage(v,0,0);
             try {
                 qrcode.decode();
+                setTimeout(captureToCanvas, 500);
             }
             catch(e) {       
                 console.log(e);
